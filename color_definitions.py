7fd174ae56bf2540f01b9306bd9546741a33c164
Hanny G <hannylicious@gmail.com>
2025-04-16 15:30:49 -0500

    Summary:WIP
    Description:

--- color_definitions.py
+++ color_definitions.py
@@ -2,19 +2,25 @@
 
 from collections import namedtuple
 from enum import Enum
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 Color = namedtuple("Color", ["name", "bg_css_class", "text_css_class"])
 
 
 class Bootstrap5(Enum):
-    BLUE = Color("Blue", "bg-primary-200", "text-primary")
-    GREEN = Color("Green", "bg-success-200", "text-success")
-    YELLOW = Color("Yellow", "bg-warning-200", "text-warning")
-    RED = Color("Red", "bg-danger-200", "text-danger")
-    PURPLE = Color("Purple", "bg-purple-200", "text-purple")
-    INDIGO = Color("Indigo", "bg-indigo-200", "text-indigo")
-    PINK = Color("Pink", "bg-pink-200", "text-pink")
-    ORANGE = Color("Orange", "bg-orange-200", "text-orange")
-    TEAL = Color("Teal", "bg-teal-200", "text-teal")
-    CYAN = Color("Cyan", "bg-cyan-200", "text-cyan")
-    GRAY = Color("Gray", "bg-gray-200", "text-gray")
+    BLUE = Color("Blue", "bg-primary", "text-primary")
+    GREEN = Color("Green", "bg-success", "text-success")
+    YELLOW = Color("Yellow", "bg-warning", "text-warning")
+    RED = Color("Red", "bg-danger", "text-danger")
+    PURPLE = Color("Purple", "bg-purple", "text-purple")
+    INDIGO = Color("Indigo", "bg-indigo", "text-indigo")
+    PINK = Color("Pink", "bg-pink", "text-pink")
+    ORANGE = Color("Orange", "bg-orange", "text-orange")
+    TEAL = Color("Teal", "bg-teal", "text-teal")
+    CYAN = Color("Cyan", "bg-cyan", "text-cyan")
+    GRAY = Color("Gray", "bg-gray", "text-gray")
+
+
+class TestTextChoices(models.TextChoices):
+    TEST = "rand-val1", _("Test Random Val")
